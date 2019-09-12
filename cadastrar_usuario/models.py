def587347ee30d4b392b0d727f86fcea47776502
Ithalo Azevedo <ithaloazvd@gmail.com>
2019-09-11 21:04:16 -0300

    "Created views and urls"
    Signed-off-by: Ithalo Azevedo <ithaloazvd@gmail.com>
    Co-authored-by: ArthurPaivaT <arthur001123@gmail.com>
    Co-authored-by> GustavoAPS <gugustavo137@gmail.com>

--- cadastrar_usuario/models.py
+++ cadastrar_usuario/models.py
@@ -4,10 +4,11 @@ from django.conf import settings
 # Create your models here.
 
 class User(models.Model):
-	matricula = models.charField(max_length=9)
-	nome = models.charField(max_length=50)
-	senha = models.charField(max_lenght=20)
-	email = models.charField(max_lenght=40)
+	matricula = models.CharField(max_length=9)
+	nome = models.CharField(max_length=50)
+	senha = models.CharField(max_length=20)
+	email = models.CharField(max_length=40)
 
 	def publish(self):
 		self.save()
+
---
+++ cadastrar_usuario/urls.py
@@ -0,0 +1,6 @@
+form django.urls import path
+form .import views
+
+urlpattern = [
+    path('', views.tela_de_cadastro, name = 'tela_de_cadastro')
+]
