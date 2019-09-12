aa4416a6a45a40b36c89d23e59baeb12fa9dfc6a
Ithalo Azevedo <ithaloazvd@gmail.com>
2019-09-11 21:04:16 -0300

    "Created views and urls"
    Signed-off-by: Ithalo Azevedo <ithaloazvd@gmail.com>
    Co-authored-by: ArthurPaivaT <arthur001123@gmail.com>
    Co-authored-by> GustavoAPS <gugustavo137@gmail.com>

--- cadastrar_usuario/views.py
+++ cadastrar_usuario/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import User
 
-# Create your views here.
+def tela_de_cadastro(request):
+    if request.method == "POST":
+        form = SubForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False)
+            user.save()
+            return redirect('tela_de_cadastro')
+
+    else:
+        form = SubForm()
+    return render(request, 'cadastrar_usuario/tela_de_cadastro.html', {'form': form})
+
+
+
+
