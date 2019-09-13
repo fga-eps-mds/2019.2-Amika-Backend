992f070d64e5587234aceb342c98d101e576a5a7
Maicon Mares <maiconmaresunb@gmail.com>
2019-09-13 10:55:03 -0300

    Correção de padrão url para path

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('^api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('cadastrar_usuario.urls'))
 ]
