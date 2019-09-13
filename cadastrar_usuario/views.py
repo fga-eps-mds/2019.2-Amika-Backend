9999947f4cdc7f19852fd9de83b852f2f8bd3bba
Maicon Mares <maiconmaresunb@gmail.com>
2019-09-13 19:48:26 -0300

    Criação da view para receber múltiplos objetos no mesmo JSON

--- cadastrar_usuario/views.py
+++ cadastrar_usuario/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import RegistrationSerializer
 from .models import Registration
+from rest_framework import generics
 
 #Criar o form SubForm
 def tela_de_cadastro(request):
@@ -21,11 +22,16 @@ def tela_de_cadastro(request):
     pass
 
 @api_view(['GET', 'POST'])
-def set_registration_list(request):
-    serializer = RegistrationSerializer(data = request.data)
+def set_registration_list(self, request):
+    data = request.data.get("items") if 'items' in request.data else request.data
+    many = isinstance(data, list)
+    print (data, many)
+    serializer = self.get_serializer(data=data, many=many)
+    #serializer = RegistrationSerializer(data = request.data)
     if request.method == 'POST':
         if serializer.is_valid():
-            matricula = RegistrationSerializer.create(serializer, request.data)
+            self.perform_create(serializer)
+            #matricula = RegistrationSerializer.create(serializer, request.data)
             return Response({"Matricula": request.data['matricula'], "turma": request.data['turma']})
         else:
             return Response({"message": "Entre com os atributos corretos!", "atributos_esperados": "matricula, turma"})
@@ -33,3 +39,21 @@ def set_registration_list(request):
         matriculas = Registration.objects.all()
         serializer = RegistrationSerializer(matriculas, many = True)
         return Response(serializer.data)
+
+        #serializer.is_valid(raise_exception=True)
+        #headers = self.get_success_headers(serializer.data)
+        #return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+class RegistrationViewSet(generics.ListCreateAPIView):
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.get("items") if 'items' in request.data else request.data
+        many = isinstance(data, list)
+        print (data, many)
+        serializer = self.get_serializer(data=data, many=many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        #headers = self.get_success_headers(serializer.data)
+        return Response({"Matricula": request.data['matricula'], "turma": request.data['turma']})
