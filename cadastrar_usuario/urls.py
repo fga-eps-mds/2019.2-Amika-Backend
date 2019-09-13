5b7c5da5ba73174331fff738cb22de32fe871b04
Maicon Mares <maiconmaresunb@gmail.com>
2019-09-13 16:20:15 -0300

    Criação da url para a view que executa o registro de múltiplas matrículas

--- cadastrar_usuario/urls.py
+++ cadastrar_usuario/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
-    path('', views.tela_de_cadastro, name = 'tela_de_cadastro')
+    path('', views.tela_de_cadastro, name = 'tela_de_cadastro'),
+    url(r'^set_registration_list/$', views.set_registration_list, name="set_registration_list"),
 ]
