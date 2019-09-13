aae3bb59218f9726e6b09f5617ac3c4812c451bc
Maicon Mares <maiconmaresunb@gmail.com>
2019-09-13 16:21:02 -0300

    Criação da view que executa o registro de múltiplas matrículas

--- cadastrar_usuario/views.py
+++ cadastrar_usuario/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from cadastrar_usuario.models import User
 from django.shortcuts import redirect
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import RegistrationSerializer
 
 #Criar o form SubForm
 def tela_de_cadastro(request):
-    if request.method == "POST":
+    """if request.method == "POST":
         form = SubForm(request.POST)
         if form.is_valid():
             user = form.save(commit = False)
@@ -13,7 +16,14 @@ def tela_de_cadastro(request):
 
     else:
         form = SubForm()
-    return render(request, 'cadastrar_usuario/tela_de_cadastro.html', {'form': form})
-
-def set_registration_list(request):
+    return render(request, 'cadastrar_usuario/tela_de_cadastro.html', {'form': form})"""
     pass
+
+@api_view(['POST'])
+def set_registration_list(request):
+    serializer = RegistrationSerializer(data = request.data)
+    if serializer.is_valid():
+        matricula = RegistrationSerializer.create(serializer, request.data)
+        return Response({"Matricula": request.data['matricula'], "turma": request.data['turma']})
+    else:
+        return Response({"message": "Entre com os atributos corretos!", "atributos_esperados": "matricula, turma"})
