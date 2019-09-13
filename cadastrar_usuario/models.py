13353c9a72eaf3a9693510f78c0c0c2a18495460
Maicon Mares <maiconmaresunb@gmail.com>
2019-09-13 10:57:22 -0300

    Criação da model e dos campos

--- cadastrar_usuario/models.py
+++ cadastrar_usuario/models.py
@@ -4,11 +4,16 @@ from django.conf import settings
 # Create your models here.
 
 class User(models.Model):
-	matricula = models.CharField(max_length=9)
-	nome = models.CharField(max_length=50)
-	senha = models.CharField(max_length=20)
-	email = models.CharField(max_length=40)
+	matricula = models.CharField(max_length=9, blank=False, null=False)
+	nome = models.CharField(max_length=50, blank=False, null=False)
+	"""O tamanho do campo deve ser aumentado, pois por questões de segurança iremos 
+	guardar o hash da senha criptografada e não a senha cadastrada"""
+	senha = models.CharField(max_length=20, blank=False, null=False)
+	email = models.CharField(max_length=40, blank=False, null=False)
 
 	def publish(self):
 		self.save()
 
+class Registration(models.Model):
+	matricula = models.CharField( max_length=9, blank=False, null=False)
+	turma = models.CharField(max_length=1, blank=False, null=False)
