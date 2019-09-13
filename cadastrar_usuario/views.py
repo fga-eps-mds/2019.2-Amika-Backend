cc3412bffd0a47a193a52ddf4a85c9e1281e7f17
Miguel Nery <nery.mik@gmail.com>
2019-09-13 17:00:18 -0300

    Criação do método get para capturar múltiplas matrículas

--- cadastrar_usuario/views.py
+++ cadastrar_usuario/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import RegistrationSerializer
+from .models import Registration
 
 #Criar o form SubForm
 def tela_de_cadastro(request):
@@ -19,11 +20,16 @@ def tela_de_cadastro(request):
     return render(request, 'cadastrar_usuario/tela_de_cadastro.html', {'form': form})"""
     pass
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 def set_registration_list(request):
     serializer = RegistrationSerializer(data = request.data)
-    if serializer.is_valid():
-        matricula = RegistrationSerializer.create(serializer, request.data)
-        return Response({"Matricula": request.data['matricula'], "turma": request.data['turma']})
+    if request.method == 'POST':
+        if serializer.is_valid():
+            matricula = RegistrationSerializer.create(serializer, request.data)
+            return Response({"Matricula": request.data['matricula'], "turma": request.data['turma']})
+        else:
+            return Response({"message": "Entre com os atributos corretos!", "atributos_esperados": "matricula, turma"})
     else:
-        return Response({"message": "Entre com os atributos corretos!", "atributos_esperados": "matricula, turma"})
+        matriculas = Registration.objects.all()
+        serializer = RegistrationSerializer(matriculas, many = True)
+        return Response(serializer.data)
