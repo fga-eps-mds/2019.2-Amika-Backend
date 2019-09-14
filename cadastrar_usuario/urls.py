33cfaab31b8460f7322ec29cee815521380cdb40
Maicon Mares <maiconmaresunb@gmail.com>
2019-09-14 16:12:45 -0300

    Mudança da url para registro de múltiplas matrículas

--- cadastrar_usuario/urls.py
+++ cadastrar_usuario/urls.py
@@ -4,6 +4,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('', views.tela_de_cadastro, name = 'tela_de_cadastro'),
-    url(r'^set_registration_list/$', views.set_registration_list, name="set_registration_list"),
-    url(r'^teste/$', views.RegistrationViewSet.as_view(), name='teste'),
+    url(r'^register_multiples/$', views.RegistrationViewSet.as_view(), name='teste'),
 ]
