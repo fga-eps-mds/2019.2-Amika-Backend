cf8afd334e21b328a612540e7f734837b6148af2
Danilow200 <danilow200@gmail.com>
2019-09-19 10:37:09 -0300

    Alteração no admin
    Co-authored-by: danilow200
    Co-authored-by: Gabrielle-Ribeiro

--- cadastrar_usuario/admin.py
+++ cadastrar_usuario/admin.py
@@ -5,10 +5,10 @@ from django.contrib.auth.admin import UserAdmin
 from .serializers import UsuarioAlunoSerializer, RegistrationSerializer, EditarAlunoSerializer
 from .models import UsuarioAluno
 
-class UsuarioAlunoAdmin(admin.ModelAdmin):
-    add_form = UsuarioAlunoSerializer
-    form = EditarAlunoSerializer
+class UsuarioProfessorAdmin(admin.ModelAdmin):
+    add_serializer = UsuarioAlunoSerializer
+    serializer = EditarAlunoSerializer
     model = UsuarioAluno
-    list_display = ["__all__"]
+    list_display = ['matricula_aluno', 'nome_aluno', 'senha_aluno', 'email_aluno']
 
-admin.site.register(UsuarioAluno, UsuarioAlunoAdmin)
+admin.site.register(UsuarioAluno, UsuarioProfessorAdmin)
