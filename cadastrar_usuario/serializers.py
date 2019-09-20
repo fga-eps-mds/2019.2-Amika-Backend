8efbbebb8e85509a160667a2725a40cde64af6f5
Maicon Mares <maiconmaresunb@gmail.com>
2019-09-20 13:20:48 -0300

    Correção do erro de retorno da view que realiza o cadastro de múltiplas matrículas

--- cadastrar_usuario/serializers.py
+++ cadastrar_usuario/serializers.py
@@ -1,5 +1,20 @@
 from rest_framework import serializers
-from .models import Registration
+from .models import UsuarioAluno, Registration
+
+class UsuarioAlunoSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = UsuarioAluno
+		fields = '__all__'
+
+	def create(self, validated_data):
+		aluno = UsuarioAluno(
+			nome_aluno=validated_data['nome_aluno'],
+			matricula_aluno=validated_data['matricula_aluno'],
+			email_aluno=validated_data['email_aluno'],
+			senha_aluno=validated_data['senha_aluno'],
+		)
+		aluno.save()
+		return aluno
 
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
