ad6abcc8a373c3bdb06c4ec2c70a861c86f54454
ArthurPaivaT <arthur001123@gmail.com>
2019-09-25 17:57:35 -0300

    Adicinando metodo de criar turma
    Co-authored-by: GustavoAPS <gugustavo137@gmail.com>
    Co-authored-by: Gabrielle-Ribeiro <gabrielleribeiro2010@gmail.com>

--- turmas/serializers.py
+++ turmas/serializers.py
@@ -5,3 +5,13 @@ class TurmaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Turma
         fields = ['id_turma', 'nome_turma', 'ano_turma', 'periodo_turma']
+
+    def create(self, validated_data):
+    	turma_atual = Turma(
+    		nome_turma = validated_data['nome_turma'],
+      		ano_turma = validated_data['ano_turma'],
+      		periodo_turma = validated_data['periodo_turma'],
+    		id_turma = validated_data['id_turma'],
+    	)
+    	turma_atual.save()
+    	return turma_atual
--- turmas/views.py
+++ turmas/views.py
@@ -5,11 +5,17 @@ from rest_framework.response import Response
 from .models import Turma
 from .serializers import TurmaSerializer
 
-@api_view(['POST'])
+@api_view(['GET'])
 def listar_turmas(request):
     queryset = Turma.objects.all()
-    serializer_class = TurmaSerializer
-    return Response({"Turma Cadastrada!"})
+    serializer_class = TurmaSerializer(queryset, many=True)
+    return Response(serializer_class.data)
 
-
-
+@api_view(['POST'])
+def criar_turmas(request):
+	serializer = TurmaSerializer(data = request.data)
+	if serializer.is_valid():
+		turma = TurmaSerializer.create(serializer, request.data)
+		return Response ({'Turma Criada!'})
+	else:
+		return Response ({'Deu errado bro'})
