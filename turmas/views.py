7d2fae5f4e0f3e8431fad7227f379af6842f4b58
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-09-25 17:09:38 -0300

    Criando funcao de listar turmas
    Co-authored-by: ArthurPaivaT <arthur001123@gmail.com>
    Co-authored-by: GustavoAPS <gugustavo137@gmail.com>

--- turmas/views.py
+++ turmas/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Turma
+from .serializers import TurmaSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def listar_turmas(request):
+    queryset = Turma.objects.all()
+    serializer_class = TurmaSerializer
+    return Response({"Turma Cadastrada!"})
+
+
+
