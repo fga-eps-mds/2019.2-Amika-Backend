474a049fa20f2645684931183308078b6a9629af
ArthurPaivaT <arthur001123@gmail.com>
2019-09-25 17:58:10 -0300

    Adicionando url para criar turma
    Co-authored-by: GustavoAPS <gugustavo137@gmail.com>
    Co-authored-by: Gabrielle-Ribeiro <gabrielleribeiro2010@gmail.com>

--- turmas/urls.py
+++ turmas/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from turmas import views
 
 urlpatterns = [
-    path('turmas', views.listar_turmas)
+    path('turmas', views.listar_turmas),
+    path('turmas_create', views.criar_turmas)
 ]
