65f06ed2684f9198c05cb0766e44602130389af7
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-09-26 18:11:50 -0300

    Adicionando django cors
    Co-authored-by: GustavoAPS <gugustavo137@gmail.com>
    Co-authored-by: ArthurPaivaT <arthur001123@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'turmas',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'core.urls'
 
