527083af784492ede3f8050f96b9a05f6f2b718f
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-09-28 14:10:24 -0300

    Incluindo name nos paths de turma

--- turmas/urls.py
+++ turmas/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from turmas import views
 
 urlpatterns = [
-    path('turmas/', views.listar_turmas),
-    path('turmas_create', views.criar_turmas),
-    path('turma_delete/<int:pk>', views.deletar_turma),
-    path('turma_edit/<int:pk>', views.editar_turma),
+    path('turmas/', views.listar_turmas, name = 'lista_turmas'),
+    path('turmas_create', views.criar_turmas, name = 'cria_turmas'),
+    path('turma_delete/<int:pk>', views.deletar_turma, name = 'deleta_turma'),
+    path('turma_edit/<int:pk>', views.editar_turma, name = 'edita_turma'),
 ]
