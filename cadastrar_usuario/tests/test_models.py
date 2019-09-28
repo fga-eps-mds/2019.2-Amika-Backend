c69e8affda35927ed10d49875608ff8757df861d
Vinícius Cantuária <cantuariavc@gmail.com>
2019-09-28 11:18:36 -0300

    Altera testes de Aluno

--- cadastrar_usuario/tests/test_models.py
+++ cadastrar_usuario/tests/test_models.py
@@ -1,19 +1,22 @@
 from django.test import TestCase
+
 from cadastrar_usuario.models import Aluno, Registro
 
 
-class AlunoCasoDeTeste(TestCase):
-    def criar_Aluno(self, matricula="190015311", nome="Carlos Marcio", senha="192837465", email="carlosmarcio@unb.br"):
-        return Aluno.objects.create(matricula=matricula, nome=nome, senha=senha, email=email)
+class AlunoCasosDeTestes(TestCase):
+    def criar_aluno(self):
+        aluno = Aluno.objects.create(
+            matricula="190015311",
+            nome="Carlos Marcio",
+            senha="192837465",
+            email="carlosmarcio@unb.br")
 
-    def test_Aluno_criacao(self):
-        aluno = self.criar_Aluno()
+        return aluno
+
+    def testa_criacao_aluno(self):
+        aluno = self.criar_aluno()
+
         self.assertTrue(isinstance(aluno, Aluno))
-        self.assertEqual(aluno.matricula, aluno.matricula)
-        self.assertEqual(aluno.nome, aluno.nome)
-        self.assertEqual(aluno.senha, aluno.senha)
-        self.assertEqual(aluno.email, aluno.email)
-
 class RegistroCasoDeTeste(TestCase):
     def criar_Registro(self, matricula="190015311", turma ="A"):
         return Registro.objects.create(matricula=matricula, turma=turma)
@@ -22,4 +25,9 @@ class RegistroCasoDeTeste(TestCase):
         aluno = self.criar_Registro()
         self.assertTrue(isinstance(aluno, Registro))
         self.assertEqual(aluno.matricula, aluno.matricula)
-        self.assertEqual(aluno.turma, aluno.turma)
+        self.assertEqual(aluno.turma, aluno.turma)        self.assertEqual(aluno.matricula, "190015311")
+        self.assertEqual(aluno.nome, "Carlos Marcio")
+        self.assertEqual(aluno.senha, "192837465")
+        self.assertEqual(aluno.email, "carlosmarcio@unb.br")
+
+
