436ffdf700ce589e4ac241305f78a25fb8ebf53d
MateusO97 <mat97_@hotmail.com>
2019-10-02 10:00:59 -0300

    Removendo metodo create do serializer

--- turmas/serializers.py
+++ turmas/serializers.py
@@ -5,12 +5,3 @@ class TurmaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Turma
         fields = ['id','nome_turma', 'ano_turma', 'periodo_turma']
-
-    def create(self, validated_data):
-    	turma_atual = Turma(
-    		nome_turma = validated_data['nome_turma'],
-      		ano_turma = validated_data['ano_turma'],
-      		periodo_turma = validated_data['periodo_turma'],
-    	)
-    	turma_atual.save()
-    	return turma_atual
