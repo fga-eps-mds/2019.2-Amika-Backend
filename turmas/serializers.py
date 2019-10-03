172abfdcaf72a88513214d615bbca1e0d4dd5e24
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-10-03 16:04:05 -0300

    Alterando serializer de Turma

--- turmas/serializers.py
+++ turmas/serializers.py
@@ -1,7 +1,15 @@
-from .models import Turma
+from .models import Turma, Periodo
 from rest_framework import serializers
+import datetime
 
 class TurmaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Turma
-        fields = ['id','nome', 'ano', 'periodo']
+        fields = ['nome']
+
+        def create(self, validated_data):
+            return Turma.objects.get_or_create(
+                nome=validated_data['nome'],
+                periodo=Periodo.objects.get_or_create(
+                    ano=datetime.date.today().year,
+                    semestre=1 if datetime.date.today().month <= 6 else 2)[0])[0]
