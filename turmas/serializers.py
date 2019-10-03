8d309c047b1bc5d97c56eb12982cfc1067069d94
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-10-03 15:01:41 -0300

    Corrigindo campos no serializer da Turma

--- turmas/serializers.py
+++ turmas/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class TurmaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Turma
-        fields = ['id','nome_turma', 'ano_turma', 'periodo_turma']
+        fields = ['id','nome', 'ano', 'periodo']
