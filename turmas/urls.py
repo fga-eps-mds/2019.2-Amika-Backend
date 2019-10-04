2f77faa4a68a15ea559c30957be653b57ba2fc97
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-10-04 19:33:00 -0300

    Modificando urls de turmas

--- turmas/urls.py
+++ turmas/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, include
 from turmas import views
 
 urlpatterns = [
-    path('', views.listar_turmas, name = 'lista_turmas'),
-    path('cadastro', views.criar_turmas, name = 'cria_turmas'),
+    path('turmas/', views.gerenciar_turmas, name = 'lista_turmas'),
     path('turma/<int:pk>', views.gerencia_turma, name = 'mostra_turma'),
 ]
 
