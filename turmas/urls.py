578e73c9de7ada9c8495bd9ddec0cb80db52af3d
Gustavo Afonso <gugustavo137@gmail.com>
2019-10-04 14:03:58 -0300

    Alterando Urls de Turma
    Co-authored-by: Gabrielle-Ribeiro <gabrielleribeiro2010@gmail.com>

--- turmas/urls.py
+++ turmas/urls.py
@@ -2,10 +2,8 @@ from django.urls import path, include
 from turmas import views
 
 urlpatterns = [
-    path('turmas/', views.listar_turmas, name = 'lista_turmas'),
-    path('turmas_create', views.criar_turmas, name = 'cria_turmas'),
-    path('turma_delete/<int:pk>', views.deletar_turma, name = 'deleta_turma'),
-    path('turma_edit/<int:pk>', views.editar_turma, name = 'edita_turma'),
+    path('', views.listar_turmas, name = 'lista_turmas'),
+    path('cadastro', views.criar_turmas, name = 'cria_turmas'),
     path('turma/<int:pk>', views.get_turma, name = 'mostra_turma'),
 ]
 
