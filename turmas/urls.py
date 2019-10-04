4be2aa761618b8e7172ecb06d7ead24325146471
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-10-04 19:34:47 -0300

    Corrigindo names das urls

--- turmas/urls.py
+++ turmas/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from turmas import views
 
 urlpatterns = [
-    path('turmas/', views.gerenciar_turmas, name = 'lista_turmas'),
-    path('turma/<int:pk>', views.gerencia_turma, name = 'mostra_turma'),
+    path('turmas/', views.gerenciar_turmas, name = 'gerencia_turmas'),
+    path('turma/<int:pk>', views.gerencia_turma, name = 'gerencia_turma'),
 ]
 
