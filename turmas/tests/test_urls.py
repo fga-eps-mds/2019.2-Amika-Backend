e765e58b106393a7bf75c85c059a6cf28a3dc858
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-10-05 10:39:29 -0300

    Alterando teste de urls

--- turmas/tests/test_urls.py
+++ turmas/tests/test_urls.py
@@ -1,25 +1,12 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from turmas.views import listar_turmas, criar_turmas, deletar_turma, editar_turma, get_turma
-from turmas.models import Turma
+from turmas.views import gerencia_turmas, gerencia_turma
 
-class TurmaUrlsCasosDeTestes(SimpleTestCase):
-    def testa_lista_turmas(self):
-        url = reverse('lista_turmas')
-        self.assertEquals(resolve(url).func, listar_turmas)
+class TesteUrls(SimpleTestCase):
+    def testa_url_gerencia_turmas(self):
+        url = reverse('gerencia_turmas')
+        self.assertEquals(resolve(url).func, gerencia_turmas)
 
-    def testa_cria_turmas(self):
-        url = reverse('cria_turmas')
-        self.assertEquals(resolve(url).func, criar_turmas)
-
-    def testa_deleta_turma(self):
-        url = reverse('deleta_turma', args = [1])
-        self.assertEquals(resolve(url).func, deletar_turma)
-
-    def testa_edita_turma(self):
-         url = reverse('edita_turma', args = [1])
-         self.assertEquals(resolve(url).func, editar_turma)
-
-    def testa_get_turma(self):
-        url = reverse('mostra_turma', args = [1])
-        self.assertEquals(resolve(url).func, get_turma)
+    def testa_url_gerencia_turma(self):
+        url = reverse('gerencia_turma', args=[1])
+        self.assertEquals(resolve(url).func, gerencia_turma)
