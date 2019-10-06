bf555abe9b7e6bc79f5b108eb0d5ff3ccc1ddd16
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-10-06 12:55:57 -0300

    Modificando teste de serializers

--- turmas/tests/test_serializers.py
+++ turmas/tests/test_serializers.py
@@ -1,37 +1,17 @@
 from django.test import TestCase
-from turmas.models import Turma
+from turmas.models import Turma, Periodo
 from turmas.serializers import TurmaSerializer
 
-class TurmaSerializerCasosDeTestes(TestCase):
-    def testa_serializer_valido_turma(self):
-        turma = Turma.objects.create(
-            nome_turma = "Felicidade",
-            ano_turma = 2019,
-            periodo_turma = 2
-        )
+class TestesTurmaSerializer(TestCase):
+    def setUp(self):
+        Periodo.objects.create(
+            ano=2019,
+            semestre=2)
 
+    def testa_criacao_de_objeto(self):
         turma_dados = {
-            'nome_turma': turma.nome_turma,
-            'ano_turma': turma.ano_turma,
-            'periodo_turma': turma.periodo_turma
+            'nome':'Felicidade'
         }
 
-        serializer_turma = TurmaSerializer(data=turma_dados)
-        self.assertTrue(serializer_turma.is_valid())
-
-    def testa_serializer_invalido_turma(self):
-        turma = Turma.objects.create(
-            nome_turma = "",
-            ano_turma = 2019,
-            periodo_turma = 2
-        )
-
-        turma_dados = {
-            'nome_turma': turma.nome_turma,
-            'ano_turma': turma.ano_turma,
-            'periodo_turma': turma.periodo_turma
-        }
-
-        serializer_turma = TurmaSerializer(data=turma_dados)
-        self.assertFalse(serializer_turma.is_valid())
-        
+        turma = TurmaSerializer().create(turma_dados)
+        self.assertTrue(isinstance(turma, Turma))
