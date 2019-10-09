48829dc7637635a38b4ed6ee0599e477e5e0ffc3
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-10-08 22:21:56 -0300

    Criando model Grupo

--- grupos/models.py
+++ grupos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Grupo(models.Model):
+    nome = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return "{}".format(self.nome)
