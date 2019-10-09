88c73a35f06cb86b9920b8f46d44f35a00305e81
Gustavo Afonso <gugustavo137@gmail.com>
2019-10-09 13:01:53 -0300

    Adicionando as models e a view do formulario

--- formulario_felicidade_autentica/models.py
+++ formulario_felicidade_autentica/models.py
@@ -2,6 +2,5 @@ from django.db import models
 from django.db.models import PROTECT
 import datetime
 
-class Formulario_Felicidade_Autentica(models.Model):
+class formulario_felicidade(models.Model):
     pontuacao = float         
-    
--- formulario_felicidade_autentica/views.py
+++ formulario_felicidade_autentica/views.py
@@ -4,4 +4,16 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import JsonResponse
-from .models import Formulario_Felicidade_Autentica
+from .models import formulario_felicidade
+from .serializers import FormuarioFelicidadeSerializer
+
+@api_view(['POST'])
+def gerencia_formulario_felicidade(request):
+
+	if request.method == 'POST':
+		serializer = FormuarioFelicidadeSerializer(data = request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		else:
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
