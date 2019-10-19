0d9dc35352e2b66f0469c02ce8de85a19907896e
Vinícius Cantuária <cantuariavc@gmail.com>
2019-10-19 16:04:16 -0300

    Refatora Backend

---
+++ amika/serializers.py
@@ -0,0 +1,95 @@
+from rest_framework import serializers
+
+from .models import *
+
+
+class TurmaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Turma
+        fields = '__all__'
+
+
+class RegistroSerializer(serializers.ModelSerializer):
+    turma = serializers.CharField(max_length=2, min_length=1)
+    ano = serializers.ReadOnlyField(source='periodo.ano')
+    semestre = serializers.ReadOnlyField(source='periodo.semestre')
+
+    class Meta:
+        model = Registro
+        fields = ['id', 'matricula', 'turma', 'ano', 'semestre']
+
+    def validate_turma(self, descricao):
+        if not Turma.objects.filter(descricao=descricao):
+            raise serializers.ValidationError("Turma não encontrada.")
+        return descricao
+
+    def create(self, validated_data):
+        return Registro.objects.get_or_create(
+            matricula=validated_data['matricula'],
+            turma=Turma.objects.get(descricao=validated_data['turma']),
+            periodo=Periodo.objects.get_or_create(ano=ano(), semestre=semestre())[0])[0]
+
+
+class AlunoSerializer(serializers.ModelSerializer):
+    grupo = serializers.CharField(max_length=100, allow_null=True, default=None)
+
+    class Meta:
+        model = Aluno
+        fields = ['id', 'username', 'first_name', 'last_name', 'password', 'grupo']
+
+    def validate_grupo(self, nome):
+        if nome and not Grupo.objects.filter(nome=nome):
+            raise serializers.ValidationError("Grupo não encontrado.")
+        return nome
+
+    def create(self, validated_data):
+        aluno = Aluno.objects.get_or_create(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            registro=Registro.objects.get(matricula=validated_data['username']),
+            grupo=None)[0]
+        aluno.set_password(validated_data['password'])
+        aluno.save()
+
+        return aluno
+
+    def update(self, aluno, validated_data):
+        if validated_data.get('password'):
+            aluno.set_password(validated_data['password'])
+
+        if validated_data.get('grupo'):
+            aluno.grupo = Grupo.objects.get(nome=validated_data['grupo'])
+
+        aluno.save()
+        return aluno
+
+
+class GrupoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Grupo
+        fields = '__all__'
+
+
+class AgendaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agenda
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if kwargs:
+            kwargs['data']['tipo'] = kwargs['data']['tipo'].capitalize()
+
+    def validate(self, data):
+        if data['data_disponibilizacao'] > data['data_encerramento']:
+            raise serializers.ValidationError({"error": "Data de disponibilização maior do que a de encerramento."})
+        return data
+
+
+def ano():
+    return date.today().year
+
+
+def semestre():
+    return 1 if date.today().month <= 6 else 2
---
+++ amika/tests/test_models.py
@@ -0,0 +1,54 @@
+from django.test import TestCase
+
+from amika.models import *
+
+
+class TestesPeriodo(TestCase):
+    def testa_str_do_objeto(self):
+        periodo = Periodo.objects.create(ano=2019, semestre=2)
+        self.assertEqual(str(periodo), "2019/2")
+
+
+class TestesTurma(TestCase):
+    def testa_str_do_objeto(self):
+        turma = Turma.objects.create(descricao="A")
+        self.assertEqual(str(turma), "A")
+
+
+class TestesRegistro(TestCase):
+    def testa_str_do_objeto(self):
+        registro = Registro.objects.create(
+            matricula=123456789,
+            turma=Turma.objects.create(descricao="A"),
+            periodo=Periodo.objects.create(ano=2019, semestre=2))
+        self.assertEqual(str(registro), "2019/2 A 123456789")
+
+
+class TestesAluno(TestCase):
+    def testa_str_do_objeto(self):
+        aluno = Aluno.objects.create(
+            username="123456789",
+            first_name="Nome",
+            last_name="Sobrenome",
+            password="123",
+            registro=Registro.objects.create(
+                matricula=123456789,
+                turma=Turma.objects.create(descricao="A"),
+                periodo=Periodo.objects.create(ano=2019, semestre=2)))
+        self.assertEqual(str(aluno), "123456789 Nome Sobrenome")
+
+class TestesGrupo(TestCase):
+    def testa_str_do_objeto(self):
+        grupo = Grupo.objects.create(nome="Grupo 1")
+        self.assertEqual(str(grupo), "Grupo 1")
+
+class TestesAgenda(TestCase):
+    def testa_str_do_objeto(self):
+
+        agenda = Agenda.objects.create(
+            nome="Atividade 2",
+            descricao="descricao agenda...",
+            tipo="Individual",
+            data_disponibilizacao="2019-09-09",
+            data_encerramento="2019-09-10")
+        self.assertEqual(str(agenda), "Atividade 2 Individual 2019-09-09/2019-09-10")
