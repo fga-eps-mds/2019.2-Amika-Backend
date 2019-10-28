6634f729617f6e0496667ce0b7c197da2f81136a
Vinícius Cantuária <cantuariavc@gmail.com>
2019-10-28 00:01:36 -0300

    Corrige conflito de migrações

--- amika/migrations/0003_formulario.py
+++ amika/migrations/0003_formulario.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('amika', '0001_initial'),
+        ('amika', '0002_humor'),
     ]
 
     operations = [
