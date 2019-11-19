fd4fb996e675a040f32abeb0210bcf3f15ab6198
Vinícius Cantuária <cantuariavc@gmail.com>
2019-11-19 14:35:41 -0200

    Corrige migração

--- amika/migrations/0011_agendarealizada.py
+++ amika/migrations/0011_agendarealizada.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('amika', '0009_auto_20191114_1204'),
+        ('amika', '0010_auto_20191115_1426'),
     ]
 
     operations = [
